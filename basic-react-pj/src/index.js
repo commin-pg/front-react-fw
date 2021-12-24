41d92974689ae6457353085534832ccdcd2454eb
khm0813 <khm.commin@gmail.com>
2021-12-24 09:38:09 +0900

    Introducing JSX

--- basic-react-pj/src/index.js
+++ basic-react-pj/src/index.js
@@ -4,10 +4,55 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+/*
+Embedding Expressions in JSX
+*/
+
+// const value = 'Embedding Expressions in JSX'
+// const element = <h1>Hello ,{value}</h1>
+
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
+
+// const user = {
+//   firstName: 'Kim',
+//   lastName: 'HyeongMin'
+// }
+
+// const element = (
+//   <h1>
+//     Hello~ , {formatName(user)}
+//   </h1>
+// )
+
+/*
+JSX is an Expression Too
+*/
+// function getGreeting(user) {
+//   if (user) {
+//     return <h1>Hello, {formatName(user)}</h1>
+//   } else {
+//     return <h1>Hello, Stranger</h1>
+//   }
+// }
+
+/*
+JSX Prevents Injection Attacks
+By default, React DOM escapes any values embedded in JSX before rendering them.
+Thus it ensures that you can never inject anything that’s not explicitly written in your application. 
+Everything is converted to a string before being rendered. This helps prevent XSS (cross-site-scripting) attacks.
+*/
+const title = response.potentiallyMaliciousInput;
+const element = <h1>{title}</h1>
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  element,
+
   document.getElementById('root')
 );
 
