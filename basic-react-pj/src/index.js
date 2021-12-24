5d510e3c33b8deeca9eccebeedfd4e467e9764b2
khm0813 <khm.commin@gmail.com>
2021-12-24 10:41:45 +0900

    state

--- basic-react-pj/src/index.js
+++ basic-react-pj/src/index.js
@@ -8,18 +8,43 @@ import RenderingApp from './pages/RenderingApp';
 
 
 
-function init() {
-  ReactDOM.render(
-    <React.StrictMode>
-      <RenderingApp />
-      <ComponentsAndPropsApp />
-    </React.StrictMode>,
-    document.getElementById('root'),
-  );
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
 
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
-setInterval(init, 1000)
 
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
