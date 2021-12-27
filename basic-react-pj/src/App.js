0eb73e121927220e36c87ab9be0fe45c3fdd3d59
khm0813 <khm.commin@gmail.com>
2021-12-27 16:14:33 +0900

    todo basic function completed..

--- basic-react-pj/src/App.js
+++ basic-react-pj/src/App.js
@@ -4,10 +4,102 @@ import Form from "./components/Form";
 import TodoItemList from "./components/TodoItemList";
 
 class App extends Component {
+
+  /*
+  초기 State 정의
+  */
+  id = 3;
+  state = {
+    input: '',
+    todos: [
+      { id: 0, text: '리액트 1', checked: false },
+      { id: 1, text: '리액트 2', checked: false },
+      { id: 2, text: '리액트 3', checked: false },
+    ]
+  }
+
+  /*
+  초기 State 정의 끝
+  */
+
+  /*
+  Function 정의
+  */
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  handleCreate = () => {
+    const { input, todos } = this.state;
+    console.log("abc", this.state)
+    this.setState({
+      input: '',
+      todos: todos.concat({
+        id: this.id++,
+        text: input,
+        checked: false,
+      })
+    })
+  }
+
+
+
+
+  handleToggle = (id) => {
+    const { todos } = this.state;
+
+    const index = todos.findIndex(todo => todo.id === id);
+    const selected = todos[index];
+    const nextTodos = [...todos];
+    nextTodos[index] = {
+      ...selected,
+      checked: !selected.checked
+    };
+
+    this.setState({
+      todos: nextTodos
+    })
+  }
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.handleCreate();
+    }
+  }
+
+
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    })
+  }
+
+  /*
+      Function 정의 끝
+  */
+
   render() {
+    const { input, todos } = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress,
+      handleToggle,
+      handleRemove,
+    } = this;
+
     return (
-      <TodoListTemplate form={<Form />}>
-        <TodoItemList />
+      <TodoListTemplate form={<Form
+        value={input}
+        onKeyPress={handleKeyPress}
+        onChange={handleChange}
+        onCreate={handleCreate}
+
+      />}>
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove} />
       </TodoListTemplate>
     )
   }
--- basic-react-pj/src/components/Form.js
+++ basic-react-pj/src/components/Form.js
@@ -1,6 +1,11 @@
 import React from "react";
 import './Form.css';
 
+/** 
+1. 텍스트 내용이 바뀌면 State 업데이트
+2. 버튼이 클릭되면 새로운 todo 생성 후 todos 업데이트
+3. 인풋에서 Enter 누르면 버튼을 클릭한것과 동일한 작업 진행
+*/
 const Form = ({ value, onChange, onCreate, onKeyPress }) => {
     return (
         <div className="form">
