4994c9bdf301e2b13796f721a7d6be6cc3ab4c4c
commin-pg <khm.commin@gmail.com>
2022-01-20 23:06:39 +0900

    clone site start

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import Auth from "../hoc/auth";
 // pages for this product
 import LandingPage from "./views/LandingPage/LandingPage.js";
-import LoginPage from "./views/LoginPage/LoginPage.js";
-import RegisterPage from "./views/RegisterPage/RegisterPage.js";
+
 import NavBar from "./views/NavBar/NavBar";
-import Footer from "./views/Footer/Footer"
+import Footer from "./views/Footer/Footer";
+import ClassPage from "./views/ClassPage/ClassPage";
+import ContactPage from "./views/ContactPage/ContactPage";
+import GalleryPage from "./views/GalleryPage/GalleryPage";
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -14,13 +16,14 @@ import Footer from "./views/Footer/Footer"
 
 function App() {
   return (
-    <Suspense fallback={(<div>Loading...</div>)}>
+    <Suspense fallback={<div>Loading...</div>}>
       <NavBar />
-      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+      <div style={{ paddingTop: "69px", minHeight: "calc(100vh - 80px)" }}>
         <Switch>
-          <Route exact path="/" component={Auth(LandingPage, null)} />
-          <Route exact path="/login" component={Auth(LoginPage, false)} />
-          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/" component={Auth(LandingPage, false)} />
+          <Route exact path="/class" component={Auth(ClassPage, false)} />
+          <Route exact path="/gallery2" component={Auth(GalleryPage, false)} />
+          <Route exact path="/contact" component={Auth(ContactPage, false)} />
         </Switch>
       </div>
       <Footer />
