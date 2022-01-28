b307e0757e9b6907b3f9284f9a4a144b1b50e03f
commin-pg <khm.commin@gmail.com>
2022-01-28 11:08:23 +0900

    google search api

--- src/components/Config.js
+++ src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const MOVIE_API_URL = "https://api.themoviedb.org/3/";
+export const MOVIE_API_KEY = "c1558d92add0b7f72649e80c62af548f";
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
