0ea087191ca4d531e3254a0e71f5a4b6c7098348
commin-pg <khm.commin@gmail.com>
2022-01-30 15:40:32 +0900

    basic package

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React, { Suspense } from 'react';
+import { Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+
+
+function App() {
+  return <div>
+      abc
+  </div>;
+}
+
+export default App;
