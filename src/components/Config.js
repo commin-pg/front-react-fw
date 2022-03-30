973f0d193db978f86a97d6dfe3915a87fd88f40e
commin-pg <khm.commin@gmail.com>
2022-03-30 23:15:04 +0900

    Finance Management System Start

--- src/components/Config.js
+++ src/components/Config.js
@@ -1,5 +1,5 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+export const USER_SERVER = '/api/auth';
 
 export const API_URL = 'https://api.themoviedb.org/3/'
 export const API_KEY = 'c1558d92add0b7f72649e80c62af548f'
